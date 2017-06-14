6d84a73e49547c94ed01844b02d1788e86e4726a
Stephen Daudell <stephen.daudell@salientsys.com>
2017-06-13 21:47:49 -0500

    Fixed an issue with not consuming the buffer completely.

--- packager/media/formats/cv/cv_media_parser.cc
+++ packager/media/formats/cv/cv_media_parser.cc
@@ -73,106 +73,121 @@ bool CVMediaParser::Parse(const uint8_t* buf, int size) {
 	// Copy new data to the back
 	memcpy(&buffer_[current_size], buf, size);
 
-	// Parsing magic bytes
-	if (state_ == State::kParsingMagic && buffer_.size() >= kMagicHeaderSize)
+
+	while ((state_ == State::kParsingMagic && buffer_.size() >= kMagicHeaderSize) ||
+		(state_ == State::kParsingHeader && buffer_.size() >= kFrameHeaderSize) ||
+		((state_ == State::kWaitingInit || state_ == State::kParsingNal) &&
+			buffer_.size() >= frame_size_))
 	{
-		// Grab the bytes
-		uint32_t magicBytes = Read32(&buffer_[0]);
-		if (magicBytes == kMagicBytes)
+
+		// Parsing magic bytes
+		if (state_ == State::kParsingMagic && buffer_.size() >= kMagicHeaderSize)
 		{
-			// All good, change state and clear data
-			state_ = State::kParsingHeader;
-			buffer_.erase(buffer_.begin(), buffer_.begin() + kMagicHeaderSize);
-		}
-		else
-		{
-			return false;
-		}
-	}
-	// Parsing header
-	if (state_ == State::kParsingHeader && buffer_.size() >= kFrameHeaderSize)
-	{
-		// Get the data
-		// {1:0|1} - key frame flag
-		// {4:size} - nalu size
-		key_frame_ = buffer_[0] == 1 ? true : false;
-		frame_size_ = Read32(&buffer_[1]);
-		// Sanity check the frame size?
-		if (frame_size_ > 0)
-		{
-			// All good, change state and clear data
-			state_ = got_config_ ? State::kParsingNal : State::kWaitingInit;
-			buffer_.erase(buffer_.begin(), buffer_.begin() + kFrameHeaderSize);
-		}
-		else
-		{
-			return false;
-		}
-	}
-	// Parsing NALU frame
-	if ((state_ == State::kWaitingInit || state_ == State::kParsingNal) &&
-		buffer_.size() >= frame_size_)
-	{
-		if (state_ == State::kWaitingInit)
-		{
-			// TODO: Figure out SPS and PPS length?
-			std::vector<uint8_t> sps_pps(buffer_.begin(), buffer_.begin() + kSpsPpsSize);
-			AVCDecoderConfigurationRecord avc_config;
-			if (!avc_config.Parse(sps_pps)) {
-				LOG(ERROR) << "Failed to parse avcc.";
+			// Grab the bytes
+			uint32_t magicBytes = Read32(&buffer_[0]);
+			if (magicBytes == kMagicBytes)
+			{
+				// All good, change state and clear data
+				state_ = State::kParsingHeader;
+				buffer_.erase(buffer_.begin(), buffer_.begin() + kMagicHeaderSize);
+			}
+			else
+			{
 				return false;
 			}
+		}
+		// Parsing header
+		if (state_ == State::kParsingHeader && buffer_.size() >= kFrameHeaderSize)
+		{
+			// Get the data
+			// {1:0|1} - key frame flag
+			// {4:size} - nalu size
+			key_frame_ = buffer_[0] == 1 ? true : false;
+			frame_size_ = Read32(&buffer_[1]);
+			// Sanity check the frame size?
+			if (frame_size_ > 0)
+			{
+				// All good, change state and clear data
+				state_ = got_config_ ? State::kParsingNal : State::kWaitingInit;
+				buffer_.erase(buffer_.begin(), buffer_.begin() + kFrameHeaderSize);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		// Parsing NALU frame
+		if ((state_ == State::kWaitingInit || state_ == State::kParsingNal) &&
+			buffer_.size() >= frame_size_)
+		{
+			if (state_ == State::kWaitingInit)
+			{
+				// TODO: Figure out SPS and PPS length?
+				std::vector<uint8_t> sps_pps(buffer_.begin(), buffer_.begin() + kSpsPpsSize);
 
-			std::string codec_string = avc_config.GetCodecString(FOURCC_avc1);
-			uint8_t nalu_length_size = avc_config.nalu_length_size();
-			uint16_t coded_width = avc_config.coded_width();
-			uint16_t coded_height = avc_config.coded_height();
-			uint32_t pixel_width = avc_config.pixel_width();
-			uint32_t pixel_height = avc_config.pixel_height();
+				AVCDecoderConfigurationRecord avc_config;
+				/*
+				if (!avc_config.Parse(sps_pps)) {
+					LOG(ERROR) << "Failed to parse avcc.";
+					return false;
+				}
+				*/
 
-			// Should be like 33333333
-			uint32_t timescale = 1 / 30 * (10 >> 8);
-			uint64_t duration = 0;
-			
-			std::shared_ptr<VideoStreamInfo> video_stream_info(new VideoStreamInfo(0, timescale,
-				duration, Codec::kCodecH264, H26xStreamFormat::kNalUnitStreamWithoutParameterSetNalus,
-				codec_string, &sps_pps[0], sps_pps.size(), coded_width, coded_height, pixel_width, pixel_height, 0, nalu_length_size,
-				std::string(), false));
+				// See: https://wiki.whatwg.org/wiki/Video_type_parameters
+				// See: https://en.wikipedia.org/wiki/H.264/MPEG-4_AVC
+				// This string means avc1, baseline (66 decimal -> 42 hex), something??, level (40 decimal -> 28 hex) which is for 1080p
+				std::string codec_string = "avc1.42c01f";// avc_config.GetCodecString(FOURCC_avc1);
+				uint8_t nalu_length_size = 0; // avc_config.nalu_length_size();
+				uint16_t coded_width = 1920; //avc_config.coded_width();
+				uint16_t coded_height = 1080; //avc_config.coded_height();
+				uint32_t pixel_width = 0; //avc_config.pixel_width();
+				uint32_t pixel_height = 0; //avc_config.pixel_height();
 
-			std::vector<std::shared_ptr<StreamInfo>> streams;
-			streams.push_back(video_stream_info);
-			init_cb_.Run(streams);
+				// Who knows
+				uint32_t timescale = 15360;
+				uint64_t duration = timescale * 60 * 10 * 30;
 
+				std::shared_ptr<VideoStreamInfo> video_stream_info(new VideoStreamInfo(0, timescale,
+					duration, Codec::kCodecH264, H26xStreamFormat::kNalUnitStreamWithoutParameterSetNalus,
+					codec_string, sps_pps.data(), sps_pps.size(), coded_width, coded_height, pixel_width, pixel_height, 0, nalu_length_size,
+					std::string(), false));
+
+				std::vector<std::shared_ptr<StreamInfo>> streams;
+				streams.push_back(video_stream_info);
+				init_cb_.Run(streams);
+
+				// All good, change state and clear data
+				state_ = State::kParsingNal;
+				buffer_.erase(buffer_.begin(), buffer_.begin() + kSpsPpsSize);
+				frame_size_ -= kSpsPpsSize;
+				got_config_ = true;
+			}
+
+			std::shared_ptr<MediaSample> stream_sample(
+				MediaSample::CopyFrom(&buffer_[0], frame_size_, key_frame_));
+
+			uint64_t ts = pts_++;
+			stream_sample->set_dts(ts);
+			stream_sample->set_pts(ts);
+			uint64_t duration = 512;
+			stream_sample->set_duration(duration);
+
+			DVLOG(3) << "Pushing frame: "
+				<< ", key=" << stream_sample->is_key_frame()
+				<< ", dur=" << stream_sample->duration()
+				<< ", dts=" << stream_sample->dts()
+				<< ", pts=" << stream_sample->pts()
+				<< ", size=" << stream_sample->data_size();
+
+			if (!new_sample_cb_.Run(0, stream_sample)) {
+				LOG(ERROR) << "Failed to process the sample.";
+				return false;
+			}
 			// All good, change state and clear data
-			state_ = State::kParsingNal;
-			buffer_.erase(buffer_.begin(), buffer_.begin() + kSpsPpsSize);
-			frame_size_ -= kSpsPpsSize;
-			got_config_ = true;
+			state_ = State::kParsingHeader;
+			buffer_.erase(buffer_.begin(), buffer_.begin() + frame_size_);
 		}
 
-		std::shared_ptr<MediaSample> stream_sample(
-			MediaSample::CopyFrom(&buffer_[0], frame_size_, key_frame_));
-		
-		uint64_t ts = pts_++;
-		stream_sample->set_dts(ts);
-		stream_sample->set_pts(ts);
-		uint64_t duration = 33333333;
-		stream_sample->set_duration(duration);
-
-		DVLOG(3) << "Pushing frame: "
-			<< ", key=" << stream_sample->is_key_frame()
-			<< ", dur=" << stream_sample->duration()
-			<< ", dts=" << stream_sample->dts()
-			<< ", pts=" << stream_sample->pts()
-			<< ", size=" << stream_sample->data_size();
-
-		if (!new_sample_cb_.Run(0, stream_sample)) {
-			LOG(ERROR) << "Failed to process the sample.";
-			return false;
-		}
-		// All good, change state and clear data
-		state_ = State::kParsingHeader;
-		buffer_.erase(buffer_.begin(), buffer_.begin() + frame_size_);
 	}
 
 	return true;
